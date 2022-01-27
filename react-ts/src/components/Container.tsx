9960c52d665c2cf8e5d00d1a9ddc3c2edea802eb
Bingo <faaa@live.com>
2022-01-27 18:23:27 +0800

    init

--- react-ts/src/components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type ContainerProps = {
-  styles: React.CSSProperties;
-};
-
-export const Container = (props: ContainerProps) => {
-  return <div style={props.styles}>Text Context goes here</div>;
-};
--- react-ts/src/components/Heading.tsx
+++
@@ -1,7 +0,0 @@
-type HeadingProps = {
-  children: string
-}
-
-export const Heading = (props: HeadingProps) => {
-  return <h2>Placeholder text</h2>
-}
